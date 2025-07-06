3b5c8acbc6004465a87dca333de3a48f969e3647
ByungJoon Lee <imjuni@gmail.com>
2025-07-06 23:35:03 +0900

    feat(deps): upgrade eslint
    - upgrade: update dependencies
    - change: replace Jest with Vitest
    - change: upgrade eslint from v8.x to v9.x
    - change: upgrade Node.js from v18 to v20

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+/**
+ * THIS FILE WAS AUTO-GENERATED.
+ * PLEASE DO NOT EDIT IT MANUALLY.
+ * ===============================
+ * IF YOU'RE COPYING THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
+ */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { configs, plugins, rules } from 'eslint-config-airbnb-extended';
+import { rules as prettierConfigRules } from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const gitignorePath = path.resolve('.', '.gitignore');
+
+const jsConfig = [
+  // ESLint Recommended Rules
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic Plugin
+  plugins.stylistic,
+  // Import X Plugin
+  plugins.importX,
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+];
+
+const nodeConfig = [
+  // Node Plugin
+  plugins.node,
+  // Airbnb Node Recommended Config
+  ...configs.node.recommended,
+];
+
+const typescriptConfig = [
+  // TypeScript ESLint Plugin
+  plugins.typescriptEslint,
+  // Airbnb Base TypeScript Config
+  ...configs.base.typescript,
+  // Strict TypeScript Config
+  rules.typescript.typescriptEslintStrict,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+    },
+  },
+];
+
+const prettierConfig = [
+  // Prettier Plugin
+  {
+    name: 'prettier/plugin/config',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  // Prettier Config
+  {
+    name: 'prettier/config',
+    rules: {
+      ...prettierConfigRules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
+
+export default [
+  // Ignore .gitignore files/folder in eslint
+  ...[
+    {
+      ignores: ['./.configs', './vitest.config.mts'],
+    },
+  ],
+  includeIgnoreFile(gitignorePath),
+  // Javascript Config
+  ...jsConfig,
+  // Node Config
+  ...nodeConfig,
+  // TypeScript Config
+  ...typescriptConfig,
+  // Prettier Config
+  ...prettierConfig,
+];
---
+++ vitest.config.mts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-default-export, import/no-extraneous-dependencies */
+
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    testTimeout: 10_000,
+    coverage: {
+      provider: 'v8', // or 'v8'
+      all: false,
+    },
+    exclude: ['node_modules', 'examples'],
+  },
+  plugins: [tsconfigPaths({ projects: ['tsconfig.json'] })],
+});
